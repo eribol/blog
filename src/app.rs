717d73cfcb23ef5c88f5b33ce738445252d8c29f
mehmeteribol <mehmeteribol@gmail.com>
2022-07-01 15:32:04 +0300

    Actix blog example for youtube tutorial

---
+++ src/app.rs
@@ -0,0 +1,109 @@
+use actix_session::Session;
+use actix_web::{Error, error, http, HttpResponse, web};
+use tera::{Context, Tera};
+use serde::*;
+use validator::{Validate};
+use bcrypt::bcrypt;
+use bcrypt::DEFAULT_COST;
+
+#[derive(Deserialize, Validate)]
+pub struct LoginUser{
+    #[validate(email)]
+    email: String,
+    password: String
+}
+#[derive(Debug, Deserialize, Validate)]
+pub struct SigninUser{
+    #[validate(email)]
+    email: String,
+    #[validate(length(min=5))]
+    username: String,
+    #[validate(must_match = "password2", length(min=6))]
+    password: String,
+    password2: String
+}
+
+#[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
+pub struct User{
+    id: i32,
+    email: String,
+    username: String,
+    password: String
+}
+pub async fn index(tmpl: web::Data<Tera>, session: Session) -> Result<HttpResponse, Error> {
+    let mut ctx = Context::new();
+    if let Some(user) = session.get::<String>("user")?{
+        ctx.insert("user", &user);
+    }
+    let a = tmpl.render("index.html", &ctx).map_err(error::ErrorInternalServerError)?;
+    //.map_err("Err");
+    Ok(HttpResponse::Ok().body(a))
+}
+
+pub async fn login(tmpl: web::Data<Tera>, session: Session) -> Result<HttpResponse, Error>{
+    if let Some(_) = session.get::<String>("user")?{
+        return Ok(redirect("/"))
+    }
+    let ctx = Context::new();
+    let a = tmpl.render("login.html", &ctx).map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().body(a))
+}
+
+pub async fn post_login(tmpl: web::Data<Tera>, form: web::Form<LoginUser>, session: Session, conn: web::Data<sqlx::SqlitePool>) -> Result<HttpResponse, Error>{
+    let login_form = form.into_inner();
+    if let Ok(_) = login_form.validate(){
+        let user: User = sqlx::query_as("select * from users where email = $1")
+            .bind(&login_form.email)
+            .fetch_one(&**conn).await.expect("AA");
+        if let Ok(_) = bcrypt::verify(&login_form.password, &user.password){
+            session.insert("user", &login_form.email)?;
+            return Ok(redirect("/"))
+        }
+    }
+    Ok(redirect("/login"))
+}
+
+pub async fn signin(tmpl: web::Data<Tera>, session: Session) -> Result<HttpResponse, Error>{
+    if let Some(_) = session.get::<String>("user")?{
+        return Ok(redirect("/"))
+    }
+    let ctx = Context::new();
+    let a = tmpl.render("signin.html", &ctx).map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().body(a))
+}
+
+pub async fn post_signin(
+    _tmpl: web::Data<Tera>,
+    form: web::Form<SigninUser>,
+    session: Session,
+    conn: web::Data<sqlx::SqlitePool>) -> Result<HttpResponse, Error>{
+    //let ctx = Context::new();
+    let user = form.into_inner();
+    if let Ok(_) = user.validate(){
+        let add_user = sqlx::query("insert into users (username, email, password) values($1,$2,$3)")
+            .bind(&user.username)
+            .bind(&user.email)
+            .bind(&bcrypt::hash(&user.password, DEFAULT_COST).expect("Şifreleme hatalı")).execute(&**conn).await;
+        match add_user{
+            Ok(_) => {
+                session.insert("user", &user.username)?;
+                return Ok(redirect("/"))
+            }
+            Err(_) => {
+                return Ok(redirect("/signin"))
+            }
+        }
+    }
+    Ok(redirect("/signin"))
+}
+pub async fn logout(session: Session) -> Result<HttpResponse, Error>{
+    session.purge();
+    Ok(redirect("/"))
+}
+
+
+fn redirect(location: &str)-> HttpResponse{
+    HttpResponse::Found()
+        .append_header((http::header::LOCATION, location))
+        .finish()
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::env;
+use actix_session::CookieSession;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
+use tera::{Tera};
+use dotenv::dotenv;
+
+use crate::app::*;
+
+mod app;
+
+#[actix_web::main]
+async fn main()-> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let db = env::var("DATABASE_URL").expect("Database_url not found in .env");
+    let conn = sqlx::SqlitePool::connect(&db).await.unwrap();
+    HttpServer::new(move || {
+        use actix_files::Files;
+        let mut templates = Tera::new("templates/**/*").expect("errors in tera templates");
+        templates.autoescape_on(vec!["tera"]);
+        App::new()
+            .wrap(Logger::default())
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            .app_data(web::Data::new(templates))
+            .app_data(web::Data::new(conn.clone()))
+            .service(web::resource("/").route(web::get().to(index)))
+            .service(
+                web::resource("/login")
+                    .route(web::post().to(post_login))
+                    .route(web::get().to(login)))
+            .service(
+                web::resource("/signin")
+                    .route(web::post().to(post_signin))
+                    .route(web::get().to(signin)))
+            .service(web::resource("/logout").route(web::get().to(logout)))
+            .service(Files::new("/static", "static").show_files_listing())
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
